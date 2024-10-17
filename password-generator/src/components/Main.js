415e4f186b90806078eeab53c3bf2c325e9bf161
Ishwarya Hidkimath Basavaraj <ishwaryahidkimath@Ishwaryas-Laptop.local>
2024-10-16 17:37:35 -0700

    first version of full app

--- password-generator/src/components/Main.js
+++ password-generator/src/components/Main.js
@@ -1,58 +1,83 @@
 import React, { useEffect, useRef, useState } from "react";
 import "../style.css";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Main = () => {
   const [password, setPassword] = useState("");
-  const [isCopy, setIsCopy] = useState(false);
-  const [length, setLength] = useState(8)
-  const [upperCase, setUpperCase] = useState(true)
-  const [lowerCase, setLowerCase] = useState(false)
-  const [number, setNumber] = useState(false)
-  const [symbol, setSymbol] = useState(false)
-  const inputRef = useRef(null)
+  const [length, setLength] = useState(10);
+
+  const minimumValue = 4;
+  const maximumValue = 50;
+  const percentage =
+    ((length - minimumValue) / (maximumValue - minimumValue)) * 100;
+
+  const [upperCase, setUpperCase] = useState(true);
+  const [lowerCase, setLowerCase] = useState(false);
+  const [number, setNumber] = useState(false);
+  const [symbol, setSymbol] = useState(false);
+  const inputRef = useRef(null);
   const handleCopy = () => {
-    window.navigator.clipboard.writeText(password)
-    toast("Password Copied")
-    inputRef.current.select()
+    window.navigator.clipboard.writeText(password);
+    toast("Password Copied");
+    inputRef.current.select();
   };
 
   const generatePassword = () => {
-    let pass = ''
-    let upperCaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    let lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz'
-    let numberList = '0123456789'
-    let symbolList = '!@#$%^&*()_+'
-    let str = ''
-    if(upperCase) {
-      str+=upperCaseChar
+    let pass = "";
+    let upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+    let numberList = "0123456789";
+    let symbolList = "!@#$%^&*()_+";
+    let str = "";
+    if (upperCase) {
+      str += upperCaseChar;
     }
-    if(lowerCase) str += lowerCaseChar
-    if(number) str += numberList
-    if(symbol) str += symbolList
+    if (lowerCase) str += lowerCaseChar;
+    if (number) str += numberList;
+    if (symbol) str += symbolList;
 
-    for(let i=0;i<length; i++) {
-      const char = Math.floor(Math.random() * str.length );
-      pass += str.charAt(char)
+    for (let i = 0; i < length; i++) {
+      const char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
     }
 
-    setPassword(pass)
+    setPassword(pass);
+  };
 
-  }
+  useEffect(() => {
+    generatePassword();
+  }, [length, upperCase, lowerCase, number, symbol]);
 
-  useEffect(()=>{
-    generatePassword()
-  },[length, upperCase, lowerCase, number, symbol])
+  const updateBar = (e) => {
+    const value = Number(e.target.value);
+    setLength(value);
+    const percentage =
+      ((value - e.target.min) / (e.target.max - e.target.min)) * 100;
+    console.log(e);
+    e.target.style.setProperty("--percentage", `${percentage}%`);
+  };
 
   return (
     <>
-      <div className="container">
-        <h1 style={{ color: "#dfd3c3" }}>
-          Generate Random Password Using Alphabets, Numbers and Special
-          Characters
+      <div className="title-bar">
+        <h1>
+          Quickly create a secure, random password using the online password
+          generator
         </h1>
-        <div className="pg_container">
+        <p>
+          Instantly generate a secure, random password with our online password
+          generator tool.
+        </p>
+      </div>
+
+      <div className="container">
+        <div className="pg-container-left">
+          <div class="image-container">
+            <img src="/passwordwallpaper.jpg" />
+          </div>
+        </div>
+        <div className="pg-container-right">
           <div className="pg-container-1">
             <input
               type="text"
@@ -60,7 +85,6 @@ const Main = () => {
               value={password}
               readOnly
               ref={inputRef}
-              
             ></input>
             <div className="buttonsWrapper">
               <div className="copy-container" onClick={handleCopy}>
@@ -73,72 +97,122 @@ const Main = () => {
               </div>
             </div>
           </div>
-          {/* <br/><br/> */}
           <div className="pg-container-2">
             <h3 className="custom-title">Customize your password</h3>
             <div className="custom-range-wrapper">
-              <label className="custom-range-label" for="password-length">Password Length</label>
-              <div className="custom-range-inner">
-                <input
-                  type="number"
-                  name="length"
-                  step="1"
-                  min="0"
-                  max="100"
-                  className="custom-range-number"
-                  id="password-length"
-                  value={length}
-                  readonly
-                ></input>
-                {/* <div className="custom-range-range">
-                  <div className="custom-range-range-bar">
-                    <div
-                      className="custom-range-range-value"
-                      style={{ width: "128px" }}
-                    ></div>
-                    <button
-                      className="custom-range-range-slider"
-                      style={{ transform: "translate(112px)" }}
-                    ></button>
-                  </div>
-                </div> */}
-                <input type="range" 
-                min={'8'}
-                max={'50'}
+              <label className="custom-range-label" for="password-length">
+                Password Length
+              </label>
+              <input
+                type="number"
+                name="length"
+                step="1"
+                min="0"
+                max="100"
+                className="custom-range-number"
+                id="password-length"
                 value={length}
-                onChange={(e)=>{
-                  console.log("eee",e.target.value)
-                  setLength(Number(e.target.value))
-                  }}/>
+                readonly
+              ></input>
+            </div>
+            <div className="custom-range">
+              <span className="range-min">{minimumValue}</span>
+              <input
+                className="slider"
+                type="range"
+                min={minimumValue}
+                max={maximumValue}
+                value={length}
+                onChange={updateBar}
+                style={{ "--percentage": `${percentage}%` }}
+              />
+              <span className="range-max">{maximumValue}</span>{" "}
+              {/* Max value */}
+            </div>
+
+            <div className="custom-options-container">
+              <div className="checkbox-wrapper">
+                <input
+                  type="checkbox"
+                  id="custom-checkbox-uppercase"
+                  checked={upperCase}
+                  name="uppercase"
+                  onChange={(e) => setUpperCase(e.target.checked)}
+                />
+                <label for="custom-checkbox-uppercase" className="toggle">
+                  <span></span>
+                </label>
+                <label
+                  className="custom-checkbox-label"
+                  for="custom-checkbox-uppercase"
+                >
+                  Uppercase
+                </label>
               </div>
 
-              {/* <input type="range" id="password-length" min="6" max="50" value="26"></input> */}
-            </div>
-            <div className="custom-options-wrapper">
-                <div className="custom-options-checkbox-wrapper">
-                    <div className="custom-options-checkbox">
-                        <input /*className="custom-checkbox-input"*/ id="custom-checkbox-uppercase" checked={upperCase} type="checkbox" name="uppercase" onChange={(e)=> setUpperCase(e.target.checked)}></input>
-                        <label className="custom-checkbox-label" for="custom-checkbox-uppercase">Uppercase</label>
-                    </div>
-                    <div className="custom-options-checkbox">
-                        <input /*className="custom-checkbox-input"*/ id="custom-checkbox-lowercase" checked={lowerCase} type="checkbox" name="lowercase" onChange={(e)=> setLowerCase(e.target.checked)}></input>
-                        <label className="custom-checkbox-label" for="custom-checkbox-lowercase">Lowercase</label>
-                    </div>
-                    <div className="custom-options-checkbox">
-                        <input /*className="custom-checkbox-input"*/ id="custom-checkbox-numbers"checked={number} type="checkbox" name="numbers" onChange={(e)=> setNumber(e.target.checked)}></input>
-                        <label className="custom-checkbox-label" for="custom-checkbox-numbers">Numbers</label>
-                    </div>
-                    <div className="custom-options-checkbox">
-                        <input /*className="custom-checkbox-input"*/ id="custom-checkbox-specialchar" checked={symbol} type="checkbox" name="specialchar" onChange={(e)=> setSymbol(e.target.checked)}></input>
-                        <label className="custom-checkbox-label" for="custom-checkbox-specialchar">Special characters</label>
-                    </div>
-                </div>
+              <div className="checkbox-wrapper">
+                <input
+                  type="checkbox"
+                  id="custom-checkbox-lowercase"
+                  checked={lowerCase}
+                  name="lowercase"
+                  onChange={(e) => setLowerCase(e.target.checked)}
+                />
+                <label for="custom-checkbox-lowercase" className="toggle">
+                  <span></span>
+                </label>
+                <label
+                  className="custom-checkbox-label"
+                  for="custom-checkbox-lowercase"
+                >
+                  Lowercase
+                </label>
+              </div>
+
+              <div className="checkbox-wrapper">
+                <input
+                  type="checkbox"
+                  id="custom-checkbox-numbers"
+                  checked={number}
+                  name="numbers"
+                  onChange={(e) => setNumber(e.target.checked)}
+                />
+                <label for="custom-checkbox-numbers" className="toggle">
+                  <span></span>
+                </label>
+                <label
+                  className="custom-checkbox-label"
+                  for="custom-checkbox-numbers"
+                >
+                  Numbers
+                </label>
+              </div>
+
+              <div className="checkbox-wrapper">
+                <input
+                  type="checkbox"
+                  id="custom-checkbox-specialchar"
+                  checked={symbol}
+                  name="specialchar"
+                  onChange={(e) => setSymbol(e.target.checked)}
+                />
+                <label for="custom-checkbox-specialchar" className="toggle">
+                  <span></span>
+                </label>
+                <label
+                  className="custom-checkbox-label"
+                  for="custom-checkbox-specialchar"
+                >
+                  Special characters
+                </label>
+              </div>
             </div>
           </div>
         </div>
-        {/* </div> */}
       </div>
-      <ToastContainer toastStyle={{ backgroundColor: "green", color:"white" }}/>
+      <ToastContainer
+        toastStyle={{ backgroundColor: "green", color: "white" }}
+      />
     </>
   );
 };
